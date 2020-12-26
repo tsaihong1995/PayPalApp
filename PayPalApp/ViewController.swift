8c6efaffd7bc9ddd219167700a0c2c5cca03a5fd
Hung-Chun, Tsai <tsaihong1995@gmail.com>
2020-12-26 12:59:54 -0500

    Initial Commit

---
+++ PayPalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PayPalApp
+//
+//  Created by Hung-Chun Tsai on 2020-12-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
